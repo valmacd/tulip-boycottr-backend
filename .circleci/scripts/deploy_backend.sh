932b344aaa54c6a583bcdfcc4d402675d8a8b045
Nicholas Kennedy <nick.kennedy@rangle.io>
2018-07-18 01:08:58 -0300

    Circle

--- .circleci/scripts/deploy_backend.sh
+++ .circleci/scripts/deploy_backend.sh
@@ -5,6 +5,6 @@ set -ex
 BUCKET=$1
 TEAM_NAME=$2
 BACKEND_ENV_NAME=$3
-aws s3 cp s3://"${BUCKET}/${TEAM_NAME}_firebase.json" "firebase_credentials.json"
+aws s3 cp s3://"${BUCKET}/${TEAM_NAME}-firebase.json" "firebase-credentials.json"
 
 eb deploy ${BACKEND_ENV_NAME}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const admin = require("firebase-admin");
-require("dotenv").config();
 
 const router = require("./api");
 const { logger } = require("./utils/logger");
 const { errorHandler } = require("./middleware/error-handler");
-const serviceAccount = require("../firebase-credentials");
+const serviceAccount = require("../firebase-credentials.json");
 
 // initialize firebase store
 admin.initializeApp({
@@ -19,7 +18,7 @@ const db = admin.firestore();
 const app = express();
 
 // The port the express app will listen on
-const port = 3000;
+const port = process.env.PORT || 8081;
 
 logger.info("🤖 Initializing middleware");
 
@@ -28,6 +27,12 @@ app.use("/", router);
 app.use(errorHandler);
 
 // Serve the application at the given port
-app.listen(port, () => {
-  logger.info(`🎧 Listening at http://localhost:${port}/`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, () => {
+    logger.info(`🎧 Listening at http://localhost:${port}/`);
+  });
+}
+
+module.exports = {
+  app
+};
