82db1998cba735406e4b7116260e12690fd5576d
Valerie MacDougall <valmacdougall@gmail.com>
2018-07-22 14:53:10 -0400

    created the get/post endpoint for the boycotts to be displayed in the frontend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const express = require("express");
+const express = require("express"),
+      bodyParser = require('body-parser');
 const morgan = require("morgan");
 const admin = require("firebase-admin");
+
+
+
 
 const router = require("./api");
 const { logger } = require("./utils/logger");
@@ -16,6 +20,23 @@ const db = admin.firestore();
 
 // Create a new express application instance
 const app = express();
+
+
+//the 2 app.use lines below are directly from npm (including comments)     
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+//1. bring in our external router
+const boycottLocationsRouter = require('./routes/BoycottLocations/boycottLocationsRouter');
+
+//2. we mount those routes on '/coffees'
+app.use('/boycotts', boycottLocationsRouter)
+
+//TO ACCESS THE GET ROUTE FOR BOYCOTT LOCATIONS:
+  // _____/boycotts/boycottLocation
 
 // The port the express app will listen on
 const port = process.env.PORT || 8081;
---
+++ src/routes/boycottLocations/boycottLocationsRouter.js
@@ -0,0 +1,61 @@
+// 1. Create an express router
+const express = require('express'),
+      router = express.Router();
+
+
+// 2. define our coffee routes on this express router
+const boycotts = [
+    {
+        name: "Starbucks",
+        address: "250 Queen St W, Toronto, ON M5V 1Z7",
+        lat: 43.6498937,
+        long: -79.3912412,
+        allBoycotts: [
+            {
+                date: 20180710,
+                text: "racsim towards customers"
+            },
+            {
+                date: 20180625,
+                text: "plastic straws"
+            },
+            {
+                date: 20180625,
+                text: "the use of non-fairtrade coffee and non-organic milk"
+            }
+        ]
+    },
+    {
+        name: "Starbucks",
+        address: "621 King St W, Toronto, ON M5V 1M5",
+        lat: 43.6473827,
+        long: -79.403075,
+        allBoycotts: [
+            {
+                date: 20180710,
+                text: "racsim towards customers"
+            },
+            {
+                date: 20180625,
+                text: "plastic straws"
+            }
+        ]
+    }
+]
+
+//GET /boycottLocation
+router.get('/boycottLocation', (req, res) => {
+    res.json(boycotts);
+});
+
+//POST /boycottLocation
+router.post('/boycottLocation', (req, res) => {
+    //take the new data in req.body, and use it to add a new boycott to our array
+    let newBoycott = req.body;
+    boycotts.push(newBoycott);
+    res.send('success');
+});
+
+// 3. now that we have configured our routes on the router we will export it to be 
+    // used in the main file
+module.exports = router;
